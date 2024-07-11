19b985871587862323423f077555c15fd0f68090
michaeltaman <mictrwork@gmail.com>
2024-07-11 12:10:23 +0300

    CORS integration backend and frontend works

--- state-machine-service/src/main/java/com/nanox/machinestate/controller/FlowController.java
+++ state-machine-service/src/main/java/com/nanox/machinestate/controller/FlowController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flows")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FlowController {
 
     private final FlowService flowService;
--- state-machine-service/src/main/java/com/nanox/machinestate/controller/StateController.java
+++ state-machine-service/src/main/java/com/nanox/machinestate/controller/StateController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/states")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StateController {
 
     @Autowired
